03f4d7c05063a2f8d24cc7d412a707ba3512d364
DismissedLight <1686188646@qq.com>
2022-02-18 20:43:09 +0800

    indexed

---
+++ Indexed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Snap.Data.Primitive
+{
+    public class Indexed<TIndex, TListElement>
+    {
+        public Indexed(TIndex index, IEnumerable<TListElement> list)
+        {
+            Index = index;
+            List = list;
+        }
+
+        public TIndex Index { get; set; }
+        public IEnumerable<TListElement> List { get; set; }
+    }
+}
--- NamedValue.cs
+++ NamedValue.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-
-namespace Snap.Data.Primitive
+﻿namespace Snap.Data.Primitive
 {
     /// <summary>
     /// 封装带有名称描述的值
     /// 在绑定枚举变量时非常有用
     /// </summary>
     /// <typeparam name="T">包含值的类型</typeparam>
-    [SuppressMessage("","CA1067")]
-    public class NamedValue<T> : IEquatable<NamedValue<T>>
+    public record NamedValue<T>
     {
         public NamedValue(string name, T value)
         {
@@ -17,12 +13,7 @@ namespace Snap.Data.Primitive
             Value = value;
         }
 
-        public string Name { get; set; }
-        public T Value { get; set; }
-
-        public bool Equals(NamedValue<T>? other)
-        {
-            return Name == other?.Name;
-        }
+        public string Name { get; }
+        public T Value { get; }
     }
 }
