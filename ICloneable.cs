a1f5d740a4397752312841af378c5fff903f1369
DismissedLight <1686188646@qq.com>
2022-03-13 16:09:21 +0800

    Create ICloneable.cs

---
+++ ICloneable.cs
@@ -0,0 +1,11 @@
+﻿namespace Snap.Data.Primitive
+{
+    public interface ICloneable<T> 
+    {
+        /// <summary>
+        /// 返回一个克隆的新对象
+        /// </summary>
+        /// <returns></returns>
+        T Clone();
+    }
+}
