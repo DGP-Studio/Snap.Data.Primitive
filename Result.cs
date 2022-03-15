74fb03180f19efd99e699af35e19e6a66e22e0a7
DismissedLight <1686188646@qq.com>
2022-03-15 15:40:26 +0800

    Create Result.cs

---
+++ Result.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Snap.Data.Primitive
+{
+    public record Result<TResult, TValue>
+    {
+        public Result(TResult isOk, TValue value)
+        {
+            IsOk = isOk;
+            Value = value;
+        }
+
+        [MemberNotNullWhen(true, nameof(Value))]
+        public TResult IsOk { get; }
+        public TValue Value { get; }
+
+        public void Deconstruct(out TResult isOk, out TValue value)
+        {
+            isOk = IsOk;
+            value = Value;
+        }
+    }
+
+    public record Result<TValue>:Result<bool,TValue>
+    {
+        public Result(bool isOk, TValue okValue, TValue notOkValue) : base(isOk, isOk ? okValue : notOkValue)
+        {
+
+        }
+    }
+}
